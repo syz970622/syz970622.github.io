4a89875c1c131f2ee552d39f2ebc1d4656e9b433
syz970622 <yunzhous@andrew.cmu.edu>
2019-10-23 23:40:31 -0400

    need pdf

--- assgn6/java.js
+++ assgn6/java.js
@@ -1,32 +1,41 @@
+// total number of items in cart
 var totalInCart = 0;
-
+// is num of item displayed
 var qtyExist = false;
 
+// highlight selected button
 function selectButton(button){
-
+    // reset button color to white
     for (i = 0; i < 4; i++){
         document.getElementById(i).style.backgroundColor = "white";
     }
+    // set selected button to grey
     document.getElementById(button).style.backgroundColor = "#DBDBDB";
+    // update glazing text
     document.getElementById("glazingText").textContent = "GLAZING: " + document.getElementById(button).innerHTML;
 }
 
+// update quantity text
 function updateQty(selection){
+    //selection option comes from https://stackoverflow.com/questions/8832375/how-to-get-selected-value-from-dropdown-list-in-javascript
     var selection = document.getElementById(selection);
     var selectedQty = Number(selection.options[selection.selectedIndex].value);
     document.getElementById("qtyText").textContent = "QTY:" + selectedQty;
 }
 
+// update total price
 function updatePrice(selection){
+    //selection option comes from https://stackoverflow.com/questions/8832375/how-to-get-selected-value-from-dropdown-list-in-javascript
     var selection = document.getElementById(selection);
     var selectedQty = Number(selection.options[selection.selectedIndex].value);
     var totalPrice = 3*selectedQty;
     document.getElementById("productPrice").textContent = "$" + totalPrice;
 }
 
+// start to display num of items
 function showCart(){
     qtyExist = true;
-    // comes from https://www.w3schools.com/jsref/met_node_appendchild.asp
+    //createTextNode comes from https://www.w3schools.com/jsref/met_node_appendchild.asp
     var node = document.createElement("LI");
     var textnode = document.createTextNode(totalInCart);
     node.setAttribute("id", "cartQty");
@@ -34,12 +43,14 @@ function showCart(){
     document.getElementById("headerOl").appendChild(node);
 }
 
+// update num of items
 function updateCart(){
     document.getElementById("cartQty").textContent = totalInCart;
 }
 
+// get selected value from drop down menu
 function getSelectedItem(selection){
-    // comes from https://stackoverflow.com/questions/8832375/how-to-get-selected-value-from-dropdown-list-in-javascript
+    //selection option comes from https://stackoverflow.com/questions/8832375/how-to-get-selected-value-from-dropdown-list-in-javascript
     var selection = document.getElementById(selection);
     var selectedQty = Number(selection.options[selection.selectedIndex].value);
     totalInCart += selectedQty;
@@ -49,6 +60,7 @@ function getSelectedItem(selection){
     updateCart();
  }
 
+// update image displayed
 function displayImg(image){
     document.getElementById("display").setAttribute("src", image +".png");
 }
